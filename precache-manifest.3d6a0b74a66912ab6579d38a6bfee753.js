db570a1765af98ea6113ba1689f2c2644372980d
Pabon Sarker <sarkerpabon@gmail.com>
2019-04-17 03:15:58 +0600

    Updates

---
+++ precache-manifest.3d6a0b74a66912ab6579d38a6bfee753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "d0cc3b0541ebfd2ad74e",
+    "url": "/contactmanager/static/js/main.9894a0f4.chunk.js"
+  },
+  {
+    "revision": "84603474d0643ec4cac1",
+    "url": "/contactmanager/static/js/2.38f16b8a.chunk.js"
+  },
+  {
+    "revision": "d0cc3b0541ebfd2ad74e",
+    "url": "/contactmanager/static/css/main.e1866c23.chunk.css"
+  },
+  {
+    "revision": "84603474d0643ec4cac1",
+    "url": "/contactmanager/static/css/2.66894cce.chunk.css"
+  },
+  {
+    "revision": "452b11efd3738738e2da180955ad0226",
+    "url": "/contactmanager/index.html"
+  }
+];
